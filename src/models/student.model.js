88ac8b1bee44de212c5f41f2b94b0edf47550b22
calebeLAR <ca6ele@gmail.com>
2023-04-13 10:59:16 -0400

    associate

---
+++ src/models/student.model.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const Student = sequelize.define('Student', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    birthDay: DataTypes.DATE,
+    active: DataTypes.BOOLEAN,
+    idCourse: DataTypes.INTEGER
+  },
+  {
+    tableName: 'students',
+    underscored: true,
+    timestamps: false
+  });
+  Student.associations = (models) => {
+    Student.belongsTo(models.course, {
+      foreignKey: 'idCourse',
+      as: 'course'
+    })
+  }
+
+  return Student;
+}
